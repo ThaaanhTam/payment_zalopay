e929056081423c43d29062b93984ef33f7c78c3c
unknown <22211tt1473@mail.tdc.edu.vn>
2024-07-23 15:25:07 +0700

    10

--- controllers/main.py
+++ controllers/main.py
@@ -16,16 +16,49 @@ class ZaloPayController(http.Controller):
     _query_status_url = "/payment/zlpay/query_status"
 
     @http.route(
-        _return_url,
+        _return_url,    
         type="http",
         methods=["GET"],
         auth="public",
         csrf=False,
     )
-    def zlpay_return_from_checkout(self, **data):
+    def zlpay_return_from_checkout(self, app_trans_id):
         """Handle redirection after payment checkout."""
         _logger.info("Sau khi thanh toán sẽ vào đâyyyyyyyyyyyyyyyyyyyyyyyyyy")
-        return request.redirect("/payment/status")
+
+
+        """Query the status of a payment."""
+        _logger.info("Truy vấn trạng thái thanh toán cho app_trans_id = %s", app_trans_id)
+        try:
+            zlpay_provider = request.env['payment.provider'].sudo().search([('code', '=', 'zlpay')], limit=1)
+            if not zlpay_provider:
+                raise ValidationError("ZaloPay provider not configured")
+
+            config = {
+                "app_id": zlpay_provider.app_id,
+                "key1": zlpay_provider.key1,
+                "key2": zlpay_provider.key2,
+                "endpoint": "https://sb-openapi.zalopay.vn/v2/query"
+            }
+
+            params = {
+                "app_id": config["app_id"],
+                "app_trans_id": app_trans_id
+            }
+
+            data = "{}|{}|{}".format(config["app_id"], params["app_trans_id"], config["key1"])
+            params["mac"] = hmac.new(config['key1'].encode(), data.encode(), hashlib.sha256).hexdigest()
+
+            response = urllib.request.urlopen(url=config["endpoint"], data=urllib.parse.urlencode(params).encode())
+            result = json.loads(response.read())
+
+            _logger.info("Kết quả truy vấn trạng thái thanh toán: %s", result)
+            return json.dumps(result)
+
+        except Exception as e:
+            _logger.error("Truy vấn trạng thái thanh toán thất bại: %s", e)
+            return json.dumps({"return_code": 0, "return_message": str(e)})
+        # return request.redirect("/payment/status")
 
     @http.route(
         _callback_url,
@@ -85,49 +118,4 @@ class ZaloPayController(http.Controller):
         return result
     
 
-    @http.route(
-        _query_status_url,
-        type="http",
-        methods=["GET"],
-        auth="public",
-        csrf=False,
-    )
-    def zlpay_query_status(self, app_trans_id):
-        """Query the status of a payment."""
-        _logger.info("Truy vấn trạng thái thanh toán cho app_trans_id = %s", app_trans_id)
-        try:
-            zlpay_provider = request.env['payment.provider'].sudo().search([('code', '=', 'zlpay')], limit=1)
-            if not zlpay_provider:
-                raise ValidationError("ZaloPay provider not configured")
-
-            config = {
-                "app_id": zlpay_provider.app_id,
-                "key1": zlpay_provider.key1,
-                "key2": zlpay_provider.key2,
-                "endpoint": "https://sb-openapi.zalopay.vn/v2/query"
-            }
-
-            params = {
-                "app_id": config["app_id"],
-                "app_trans_id": app_trans_id
-            }
-
-            data = "{}|{}|{}".format(config["app_id"], params["app_trans_id"], config["key1"])
-            params["mac"] = hmac.new(config['key1'].encode(), data.encode(), hashlib.sha256).hexdigest()
-
-            response = urllib.request.urlopen(url=config["endpoint"], data=urllib.parse.urlencode(params).encode())
-            result = json.loads(response.read())
-
-            _logger.info("Kết quả truy vấn trạng thái thanh toán: %s", result)
-            return json.dumps(result)
-
-        except Exception as e:
-            _logger.error("Truy vấn trạng thái thanh toán thất bại: %s", e)
-            return json.dumps({"return_code": 0, "return_message": str(e)})
-    
-
-
-
-
-    
-    
+   
