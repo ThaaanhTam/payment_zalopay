34bc198a0a287a6a4365b7c5f7ec0d336b92c96a
unknown <22211tt1473@mail.tdc.edu.vn>
2024-07-23 14:25:07 +0700

    10

--- controllers/main.py
+++ controllers/main.py
@@ -58,7 +58,7 @@ class ZaloPayController(http.Controller):
                 # Thanh toán thành công
                 # Cập nhật trạng thái cho đơn hàng
                 dataJson = json.loads(cbdata['data'])
-                app_trans_id = "240723_141588"
+                app_trans_id = dataJson['app_trans_id']
                 _logger.info("Cập nhật trạng thái đơn hàng = success cho app_trans_id = %s", app_trans_id)
               
               
@@ -66,7 +66,7 @@ class ZaloPayController(http.Controller):
                 for tx in all_transactions:
                     _logger.info("Giao dịch hiện có: %s với app_trans_id: %s", tx.id, tx.app_trans_id)
                  # Tìm giao dịch tương ứng với app_trans_id
-                tx = request.env['payment.transaction'].sudo().search([('app_trans_id', '=', app_trans_id)], limit=1)
+                tx = request.env['payment.transaction'].sudo().search([('240723_141588', '=', app_trans_id)], limit=1)
                 if tx:
                     tx._set_done()
                     # tx._reconcile_after_done()
