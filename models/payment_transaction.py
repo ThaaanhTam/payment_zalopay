0e3d85908e9f94ee567e96787a37f66f7b38e3e2
unknown <22211tt1473@mail.tdc.edu.vn>
2024-08-08 09:19:00 +0700

    A

--- models/payment_transaction.py
+++ models/payment_transaction.py
@@ -1,88 +1,93 @@
-import hashlib
-import hmac
 import logging
 import json
+import hmac
+import hashlib
 import urllib.parse
-from werkzeug.exceptions import Forbidden
-from odoo import _, http
-from odoo.exceptions import ValidationError
-from odoo.http import request
+from datetime import datetime
+from time import time
+import random
 
+from werkzeug import urls
+from odoo import _, api, fields, models
+from odoo.addons.payment_zalopay import const
+from odoo.http import request
 _logger = logging.getLogger(__name__)
 
-class ZaloPayController(http.Controller):
-    _return_url = "/payment/zalopay/return"
-    _callback_url = "/payment/zalopay/callback"
-    @http.route(
-        _return_url,
-        type="http",
-        methods=["GET"],
-        auth="public",
-        csrf=False,
-    )
-    def zalopay_return_from_checkout(self, **data):
-        """Handle redirection after payment checkout."""
-        _logger.info("Sau khi thanh toán sẽ vào đâyyyyyyyyyy")
-        return request.redirect("/payment/status")
+class PaymentTransaction(models.Model):
+    _inherit = "payment.transaction"
+    app_trans_id = fields.Char(string="App Transaction ID")
+    def _get_specific_rendering_values(self, processing_values):
+        res = super()._get_specific_rendering_values(processing_values)
+        if self.provider_code != "zalopay":
+            return res
 
-    @http.route(
-        _callback_url,
-        type="json",
-        auth="public",
-        methods=["POST"],
-        csrf=False,
-    )
-    def zalopay_callback(self):
-        """Xử lý callback từ ZaloPay."""
-        result = {}
-        logging.info("xử lý callback")
+        base_url = self.provider_id.get_base_url()
+        int_amount = int(self.amount)
+
+        # Tạo ID giao dịch và thời gian hiện tại
+        trans_id = random.randrange(1000000)
+        app_time = int(round(time() * 1000))  # Thời gian hiện tại tính bằng mili giây
+
+       # Lấy các mục từ đơn hàng
+        order_items = []
+        for line in self.invoice_ids.mapped('invoice_line_ids'):
+            item = {
+                "id": line.id,
+                "name": line.name,
+                "price": int(line.price_unit * 100)  # Giá sản phẩm tính bằng xu
+            }
+            order_items.append(item)
+        # Tạo đơn hàng
+        order = {
+            "app_id": self.provider_id.appid,
+            "app_trans_id": "{:%y%m%d}_{}".format(datetime.today(), trans_id),
+            "app_user": self.provider_id.app_user,
+            "app_time": app_time,
+            "embed_data": json.dumps({"redirecturl": urls.url_join(base_url, '/payment/zalopay/return')}),
+            "item": json.dumps(order_items),
+            "amount": int_amount,
+            "description": f"Nếu đọc được này thì đã lấy đc qr thành công #{trans_id}",
+            "bank_code": "",
+            "callback_url": urls.url_join(base_url, '/payment/zalopay/callback'),  # URL callback
+        }
+        _logger.info(urls.url_join(base_url, '/payment/zalopay/callback'))
+            
+        # Chuỗi dữ liệu để tạo chữ ký
+        data = "{}|{}|{}|{}|{}|{}|{}".format(
+            order["app_id"], order["app_trans_id"], order["app_user"],
+            order["amount"], order["app_time"], order["embed_data"], order["item"]
+        )
+
+        # Tạo chữ ký (mac)
+        order["mac"] = hmac.new(self.provider_id.key1.encode(), data.encode(), hashlib.sha256).hexdigest()
+        
+
+        # Gửi yêu cầu tạo đơn hàng đến ZaloPay
         try:
-            raw_data = request.httprequest.get_data()
-            cbdata = json.loads(raw_data)
-            _logger.info("Dữ liệu callback nhận được: ")
-            _logger.info("Dữ liệu callback nhận được: %s", cbdata)
-            _logger.info(cbdata)
-
-            zalopay_provider = request.env['payment.provider'].sudo().search([('code', '=', 'zalopay')], limit=1)
-            key2 = zalopay_provider.key2
-
-            mac = hmac.new(key2.encode(), cbdata['data'].encode(), hashlib.sha256).hexdigest()
-
-            # Kiểm tra callback hợp lệ (đến từ ZaloPay server)
-            if mac != cbdata['mac']:
-                # Callback không hợp lệ
-                _logger.info("Không nhận được dữ liệu JSON từ ZaloPay")
-                result['return_code'] = -1
-                result['return_message'] = 'mac not equal'
-            else:
-                # Thanh toán thành công
-                # Cập nhật trạng thái cho đơn hàng
-                dataJson = json.loads(cbdata['data'])
-                app_trans_id = dataJson['app_trans_id']
-                _logger.info("Cập nhật trạng thái đơn hàng = success cho app_trans_id = %s", app_trans_id)
-                    
-                all_transactions = request.env['payment.transaction'].sudo().search([])
-                for tx in all_transactions:
-                    _logger.info("Giao dịch hiện có: %s với app_trans_id: %s", tx.id, tx.app_trans_id)
-                 # Tìm giao dịch tương ứng với app_trans_id
-                tx = request.env['payment.transaction'].sudo().search([('app_trans_id', '=', app_trans_id)], limit=1)
-                if tx:
-                    tx._set_done()
-                    tx._reconcile_after_done()
-                    _logger.info("Đã cập nhật trạng thái đơn hàng thành công cho app_trans_id = %s", app_trans_id)
-                    result['return_code'] = 1
-                    result['return_message'] = 'success'
-                else:
-                    _logger.warning("Không tìm thấy giao dịch với app_trans_id = %s", app_trans_id)
-                    result['return_code'] = -1
-                    result['return_message'] = 'Transaction not found'
+            response = urllib.request.urlopen(url="https://sb-openapi.zalopay.vn/v2/create", data=urllib.parse.urlencode(order).encode())
+            result = json.loads(response.read())
+            _logger.info("Tạo hóa đơn thành công 13: %s", result)
+            # Cập nhật trường app_trans_id
+            self.write({
+                'app_trans_id': order['app_trans_id']
+            })
+            
         except Exception as e:
-            _logger.error("Xử lý callback ZaloPay thất bại: %s", e)
-            result['return_code'] = 0  # ZaloPay server sẽ callback lại (tối đa 3 lần)
-            result['return_message'] = str(e)
-        _logger.info("Kết thúc xử lý callback ZaloPay với kết quả: %s", result)
-        # Thông báo kết quả cho ZaloPay server
-        return result
+            _logger.error("ZaloPay create order failed: %s", e)
+            raise ValidationError(_("fffffffffffffffffffffffff: %s") % e)
+
+        # Trả về các giá trị cần thiết để hiển thị
+        rendering_values = {
+            "api_url": result.get("order_url"),
+        }
+         # Xử lý phản hồi từ ZaloPay
+        
+        return rendering_values
+   
+        
+    
+
+
     
 
     
